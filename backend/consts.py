0a22f1325aa8a1a615f89f9efb8533579209e33c
letienne <loicetienne@gmail.com>
2022-08-08 21:32:18 +0200

    Update consts.py

--- backend/consts.py
+++ backend/consts.py
@@ -15,8 +15,8 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 # We can customize generation parameters (see https://huggingface.co/blog/how-to-generate)
 GEN_TOP_K = None
 GEN_TOP_P = None
-TEMPERATURE  = None
-COND_SCALE = 10.0
+TEMPERATURE  = .9
+COND_SCALE = 9.0
 
 class ModelSize(Enum):
     MINI = "Mini"
